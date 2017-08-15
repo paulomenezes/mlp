7bd7c998335401b19225a77becc517fc6e98b5a3
Paulo Menezes <paulo.hgmenezes@gmail.com>
2017-08-15 20:22:05 -0300

    Normalize data

--- back-propagation.ts
+++ back-propagation.ts
@@ -18,10 +18,7 @@ export class BackPropagation {
   previousWeightDelta: number[][][];
 
   constructor(layerSizes: number[], transferFunctions: TransferFunction[]) {
-    if (
-      transferFunctions.length !== layerSizes.length ||
-      transferFunctions[0] !== TransferFunction.NONE
-    ) {
+    if (transferFunctions.length !== layerSizes.length || transferFunctions[0] !== TransferFunction.NONE) {
       throw new TypeError('Invalid parameters');
     }
 
@@ -61,11 +58,7 @@ export class BackPropagation {
       this.previousWeightDelta[l] = [];
 
       // Length = layerSize[l]
-      for (
-        var i = 0;
-        i < (l === 0 ? this.inputSize : this.layerSize[l - 1]);
-        i++
-      ) {
+      for (var i = 0; i < (l === 0 ? this.inputSize : this.layerSize[l - 1]); i++) {
         this.weight[l][i] = [];
         this.previousWeightDelta[l][i] = [];
       }
@@ -81,11 +74,7 @@ export class BackPropagation {
         this.delta[l][j] = 0.0;
       }
 
-      for (
-        var i = 0;
-        i < (l === 0 ? this.inputSize : this.layerSize[l - 1]);
-        i++
-      ) {
+      for (var i = 0; i < (l === 0 ? this.inputSize : this.layerSize[l - 1]); i++) {
         for (var j = 0; j < this.layerSize[l]; j++) {
           this.weight[l][i][j] = Gaussian.getNormalGaussian();
           this.previousWeightDelta[l][i][j] = 0.0;
@@ -107,23 +96,14 @@ export class BackPropagation {
     for (var l = 0; l < this.layerCount; l++) {
       for (var j = 0; j < this.layerSize[l]; j++) {
         let sum = 0.0;
-        for (
-          var i = 0;
-          i < (l === 0 ? this.inputSize : this.layerSize[l - 1]);
-          i++
-        ) {
-          sum +=
-            this.weight[l][i][j] *
-            (l === 0 ? input[i] : this.layerOutput[l - 1][i]);
+        for (var i = 0; i < (l === 0 ? this.inputSize : this.layerSize[l - 1]); i++) {
+          sum += this.weight[l][i][j] * (l === 0 ? input[i] : this.layerOutput[l - 1][i]);
         }
 
         sum += this.bias[l][j];
 
         this.layerInput[l][j] = sum;
-        this.layerOutput[l][j] = TransferFunctions.evaluate(
-          this.transferFunction[l],
-          sum
-        );
+        this.layerOutput[l][j] = TransferFunctions.evaluate(this.transferFunction[l], sum);
       }
     }
 
@@ -135,16 +115,8 @@ export class BackPropagation {
     return output;
   }
 
-  train(
-    input: number[],
-    desired: number[],
-    trainingRate: number,
-    momentum: number
-  ) {
-    if (
-      input.length !== this.inputSize ||
-      desired.length !== this.layerSize[this.layerCount - 1]
-    ) {
+  train(input: number[], desired: number[], trainingRate: number, momentum: number) {
+    if (input.length !== this.inputSize || desired.length !== this.layerSize[this.layerCount - 1]) {
       console.log(desired.length, this.layerSize[this.layerCount - 1]);
       throw new TypeError('Invalid input');
     }
@@ -165,10 +137,7 @@ export class BackPropagation {
           this.delta[l][k] = output[k] - desired[k];
           error += Math.pow(this.delta[l][k], 2);
 
-          this.delta[l][k] *= TransferFunctions.evaluateDerivative(
-            this.transferFunction[l],
-            this.layerInput[l][k]
-          );
+          this.delta[l][k] *= TransferFunctions.evaluateDerivative(this.transferFunction[l], this.layerInput[l][k]);
         }
       } else {
         // Hidden layer
@@ -178,10 +147,7 @@ export class BackPropagation {
             sum += this.weight[l + 1][i][j] * this.delta[l + 1][j];
           }
 
-          sum *= TransferFunctions.evaluateDerivative(
-            this.transferFunction[l],
-            this.layerInput[l][i]
-          );
+          sum *= TransferFunctions.evaluateDerivative(this.transferFunction[l], this.layerInput[l][i]);
 
           this.delta[l][i] = sum;
         }
@@ -190,17 +156,10 @@ export class BackPropagation {
 
     // Update the weights and biases
     for (var l = 0; l < this.layerCount; l++) {
-      for (
-        var i = 0;
-        i < (l === 0 ? this.inputSize : this.layerSize[l - 1]);
-        i++
-      ) {
+      for (var i = 0; i < (l === 0 ? this.inputSize : this.layerSize[l - 1]); i++) {
         for (var j = 0; j < this.layerSize[l]; j++) {
           weightDelta =
-            trainingRate *
-              this.delta[l][j] *
-              (l === 0 ? input[i] : this.layerOutput[l - 1][i]) +
-            momentum * this.previousWeightDelta[l][i][j];
+            trainingRate * this.delta[l][j] * (l === 0 ? input[i] : this.layerOutput[l - 1][i]) + momentum * this.previousWeightDelta[l][i][j];
 
           this.weight[l][i][j] -= weightDelta;
           this.previousWeightDelta[l][i][j] = weightDelta;
--- main.ts
+++ main.ts
@@ -1,63 +1,80 @@
+const ChartjsNode = require('chartjs-node');
+
 import { BackPropagation } from './back-propagation';
 import { TransferFunction } from './transfer-function';
 
 import * as fs from 'fs';
 
-let input: number[][] = [];
-let output: number[][] = [];
+const readFile = (name: string) => {
+  let input: number[][] = [];
+  let output: number[][] = [];
 
-const data = fs.readFileSync('./data/training.txt').toString().split('\n');
-data.forEach(line => {
-  let inputLine: number[] = [];
-  line.substr(line.indexOf(',') + 1).split(',').forEach(value => {
-    inputLine.push(+value);
+  const data = fs.readFileSync(name).toString().split('\n');
+  data.forEach(line => {
+    let inputLine: number[] = [];
+    line.substr(line.indexOf(',') + 1).split(',').forEach(value => {
+      inputLine.push(+value);
+    });
+
+    if (inputLine.length === 16) {
+      input.push(inputLine);
+
+      let outputLine: number[] = [];
+      for (var i = 0; i < 26; i++) {
+        outputLine[i] = 0;
+      }
+
+      outputLine[+line.substr(0, line.indexOf(','))] = 1;
+      output.push(outputLine);
+    }
   });
 
-  if (inputLine.length === 16) {
-    input.push(inputLine);
+  return [input, output];
+};
 
-    let outputLine: number[] = [];
-    for (var i = 0; i < 26; i++) {
-      outputLine[i] = 0;
-    }
+const trainingData = readFile('./data/training.txt');
 
-    outputLine[+line.substr(0, line.indexOf(','))] = 1;
-    output.push(outputLine);
-  }
-});
+let input: number[][] = trainingData[0];
+let output: number[][] = trainingData[1];
 
-let network = new BackPropagation(
-  [16, 10, 10, 10, 26],
-  [
-    TransferFunction.NONE,
-    TransferFunction.SIGMOID,
-    TransferFunction.SIGMOID,
-    TransferFunction.SIGMOID,
-    TransferFunction.LINEAR
-  ]
-);
+let network = new BackPropagation([16, 26, 26], [TransferFunction.NONE, TransferFunction.SIGMOID, TransferFunction.SIGMOID]);
 
-const maxCount = 50000;
+const maxCount = 500;
+const size = input.length;
+
 let error = 0.0;
 let count = 0;
+
+let epochs = [];
+let values = [];
 
 do {
   count++;
   error = 0.0;
 
-  for (var i = 0; i < 4; i++) {
+  for (var i = 0; i < size; i++) {
     error += network.train(input[i], output[i], 0.15, 0.1);
   }
 
-  // Show progress
-  if (count % 500 === 0) {
-    console.log(`Epoch ${count} completed with error ${error}`);
-  }
-} while (error > 0.00001 && count <= maxCount);
+  error = error / size;
 
+  // Show progress
+  epochs.push(count);
+  values.push(error);
+  console.log(`Epoch ${count} completed with error ${error}`);
+} while (error > 0.1 && count <= maxCount);
+
+fs.writeFile('./output/result.txt', values);
+
+const testData = readFile('./data/test.txt');
+let inputTest: number[][] = testData[0];
+let outputTest: number[][] = testData[1];
+
+let correct = 0;
 let networkOutput: number[];
-for (var i = 0; i < 10; i++) {
-  networkOutput = network.run(input[i]);
+
+for (var i = 0; i < inputTest.length; i++) {
+  networkOutput = network.run(inputTest[i]);
   let highest = 0;
   let index = 0;
 
@@ -69,23 +86,44 @@ for (var i = 0; i < 10; i++) {
       index = j;
     }
 
-    if (output[i][j] === 1) {
+    if (outputTest[i][j] === 1) {
       rightIndex = j;
     }
   }
 
-  console.log(
-    String.fromCharCode(65 + rightIndex),
-    String.fromCharCode(65 + index)
-  );
+  if (rightIndex === index) {
+    correct++;
+  }
 }
 
-/*
- * XOR-Gate Example
-let network = new BackPropagation(
-  [2, 2, 1],
-  [TransferFunction.NONE, TransferFunction.SIGMOID, TransferFunction.LINEAR]
-);
+console.log(correct, inputTest.length);
+
+var chartNode = new ChartjsNode(1920, 1080);
+chartNode
+  .drawChart({
+    type: 'line',
+    data: {
+      labels: epochs,
+      datasets: [
+        {
+          label: 'Error',
+          data: values,
+          borderColor: 'rgb(255, 0, 0)',
+          fill: false
+        }
+      ]
+    },
+    options: {
+      width: 1920,
+      height: 1080
+    }
+  })
+  .then((streamResult: any) => {
+    return chartNode.writeImageToFile('image/png', './output/MSE.png');
+  });
+
+/* XOR-Gate Example
+let network = new BackPropagation([2, 2, 1], [TransferFunction.NONE, TransferFunction.SIGMOID, TransferFunction.LINEAR]);
 
 let input: number[][] = [[0, 0], [0, 1], [1, 0], [1, 1]];
 let output: number[][] = [[0], [1], [1], [0]];
@@ -111,8 +149,6 @@ do {
 let networkOutput: number[];
 for (var i = 0; i < 4; i++) {
   networkOutput = network.run(input[i]);
-  console.log(
-    `Case ${i} ${input[i][0]} xor ${input[i][1]} = ${networkOutput[0]}`
-  );
+  console.log(`Case ${i} ${input[i][0]} xor ${input[i][1]} = ${networkOutput[0]}`);
 }
 */
